848059c303bf69c4f52cd90c38e621d26a7083c0
Anikesh0204 <anikeshkulal@gmail.com>
2021-10-05 12:09:11 +0530

    Initial commit

---
+++ module1.mjs
@@ -0,0 +1,7 @@
+import dza, {b, c, d} from './module2.mjs'
+console.log(dza);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a = "Anikesh";
+const b = "Krishna";
+const d = "Tushar";
+const c = "Harshit";
+
+export default a;
+export {b};
+export {c};
+export {d};
